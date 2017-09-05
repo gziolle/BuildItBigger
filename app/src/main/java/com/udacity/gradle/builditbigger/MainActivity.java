f438049b5b883029056f220ea58857985f777a21
gziolle <gziolle@gmail.com>
2017-09-04 21:52:46 -0300

    Adding a Java library to tell jokes.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.JokeWizard;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,8 +42,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        JokeWizard wizard = new JokeWizard();
+        String joke = wizard.getJoke();
+        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
     }
-
-
 }
---
+++ jokewizard/src/main/java/com/example/JokeWizard.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class JokeWizard {
+
+    public String getJoke(){
+        return "A joke is a joke is a joke!!";
+    }
+
+}
