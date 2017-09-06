a7e1d10c5d2228a9fb667dbb224096dd4d35e0a7
gziolle <gziolle@gmail.com>
2017-09-05 21:20:02 -0300

    Adding the Joke Server - backend.

---
+++ jokeserver/src/main/java/com/gziolle/builditbigger/JokeEndpoint.java
@@ -0,0 +1,40 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.gziolle.builditbigger;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+
+import javax.inject.Named;
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(
+        name = "jokeApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "builditbigger.gziolle.com",
+                ownerName = "builditbigger.gziolle.com",
+                packagePath = ""
+        )
+)
+public class JokeEndpoint {
+
+    /**
+     * A simple endpoint method that takes a name and says Hi back
+     */
+    @ApiMethod(name = "getJoke")
+    public JokeBean getJoke(@Named("name") String name) {
+        JokeBean response = new JokeBean();
+        response.setData("Hi, " + name);
+
+        return response;
+    }
+
+}
